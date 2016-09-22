71b37a52d09d51aba7c799f177a73fa981183395
Otis Bailey <Bonafide.aa@gmail.com>
2016-09-21 21:50:44 -0500

    Need to parse filenames

--- DCSortment/Program.cs
+++ DCSortment/Program.cs
@@ -11,79 +11,120 @@ namespace DCSortment
 {
     class Program
     {
-
+        static string _namingUpperPosition = "AA";
+        static string _namingLowerPosition = "aa";
 
         static void Main(string[] args)
         {
+            string currentDirectory = System.AppDomain.CurrentDomain.BaseDirectory;
 
+            //List Variables
+            List<House> houses = new List<House>();
+            List<House> SortedHouseList;
+            List<string> fileNames;
 
             //Excel Variables
             Excel.Application xlApp = new Excel.Application();
-            Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(@"C:\Users\Otis\Documents\Visual Studio 2015\Projects\DCSortment\inputTest.xlsx");
-            Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
-            Excel.Range xlRange = xlWorksheet.UsedRange;
-            List<House> houses = new List<House>();
-            List<House> SortedList;
 
-            //Reading and storing input dataset
-            int rowCount = xlRange.Rows.Count;
-            int colCount = xlRange.Columns.Count;
-            string input = "";
-            double num;
-
-
-            for (int i = 2; i <= rowCount; i++)
+            try
             {
-                for (int j = 1; j <= colCount; j++)
-                {
+                Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(currentDirectory + "inputTest.xlsx");
+                Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
+                Excel.Range xlRange = xlWorksheet.UsedRange;
 
-                    
-                    if (xlRange.Cells[i, j] != null && xlRange.Cells[i, j].Value2 != null)
+                //Reading and storing input dataset
+                string input = "";
+                double num;
+                int rowCount = xlRange.Rows.Count;
+                int colCount = xlRange.Columns.Count;
+
+
+                for (int i = 2; i <= rowCount; i++)
+                {
+                    for (int j = 1; j <= colCount; j++)
                     {
 
-                        //If the current value being parsed is a number
-                        if (double.TryParse(xlRange.Cells[i, j].Value2.ToString(), out num))
-                        {
-                            // Make a new House object and set its name and rating, then add it to the list of houses.
-                            House temp = new House();
-                            temp.houseName = input;
-                            temp.rating = num;
-                            houses.Add(temp);
-                        }
 
-                        // If the current value isn't a number then it must be a house name so we store it while we wait for its rating.
-                        else
+                        if (xlRange.Cells[i, j] != null && xlRange.Cells[i, j].Value2 != null)
                         {
-                            input = xlRange.Cells[i, j].Value2.ToString();
+
+                            //If the current value being parsed is a number
+                            if (double.TryParse(xlRange.Cells[i, j].Value2.ToString(), out num))
+                            {
+                                // Make a new House object and set its name and rating, then add it to the list of houses.
+                                House temp = new House();
+                                temp.houseName = input;
+                                temp.rating = num;
+                                houses.Add(temp);
+                            }
+
+                            // If the current value isn't a number then it must be a house name so we store it while we wait for its rating.
+                            else
+                            {
+                                input = xlRange.Cells[i, j].Value2.ToString();
+                            }
                         }
                     }
+
+
                 }
 
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+
+
+                Marshal.ReleaseComObject(xlRange);
+                Marshal.ReleaseComObject(xlWorksheet);
+
+                xlWorkbook.Close();
+                Marshal.ReleaseComObject(xlWorkbook);
+
+                xlApp.Quit();
+                Marshal.ReleaseComObject(xlApp);
+
+                //End of Reading and storing input
+            }
+
+            catch (COMException)
+            {
+
+                Console.WriteLine("\nThe required dataset .xlsx file was not found. Please ensure \"DataSet.xlsx\" is in the current directory as the executeable.");
 
             }
 
-            GC.Collect();
-            GC.WaitForPendingFinalizers();
-
-
-            Marshal.ReleaseComObject(xlRange);
-            Marshal.ReleaseComObject(xlWorksheet);
-
-            xlWorkbook.Close();
-            Marshal.ReleaseComObject(xlWorkbook);
-
-            xlApp.Quit();
-            Marshal.ReleaseComObject(xlApp);
-
-            //End of Reading and storing input
 
             //Sort list
 
-            SortedList = houses.OrderByDescending(house => house.rating).ThenBy(house => house.houseName).ToList();
+            SortedHouseList = houses.OrderByDescending(house => house.rating).ThenBy(house => house.houseName).ToList();
 
             //
 
+            foreach (House house in SortedHouseList) {
+                Console.WriteLine(house.houseName + " -> " + house.rating);
+            }
 
+
+            try
+            {
+                fileNames = Directory.GetFiles(currentDirectory + "Files\\").ToList();
+                string test = currentDirectory + "Files\\";
+           
+
+                foreach (string filename in fileNames)
+                {
+                    Console.WriteLine(filename);
+
+                }
+
+           
+            }
+
+            catch (DirectoryNotFoundException)
+            {
+                Console.WriteLine("\nThe working file directory was not found. Please Ensure that the folder named \"Files\" has been created in the same directory as the program.)");
+            }
+
+            
 
 
 
