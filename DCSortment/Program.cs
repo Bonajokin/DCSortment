c3fb6b9880beff2b9ab8aa728615410247f833bc
Otis Bailey <Bonafide.aa@gmail.com>
2016-09-30 09:47:15 -0500

    Revert "Fixed output to have ratings in 0.00 format" Committed to the wrong branch
    This reverts commit 19624bad72c828985dc758b4d2031483b4c74259.

--- DCSortment/Program.cs
+++ DCSortment/Program.cs
@@ -263,7 +263,7 @@ namespace DCSortment
                                     }
                                     else
                                     {
-                                        renameName = _namingUpperPosition + "_" + (currentHouse.rating).ToString("0.00");
+                                        renameName = _namingUpperPosition + "_" + currentHouse.rating;
                                     }
 
                                     incrementNamingConvention(_namingUpperPosition, true);
@@ -284,7 +284,7 @@ namespace DCSortment
                                     }
                                     else
                                     {
-                                       renameName = _namingLowerPosition + "_" + (currentHouse.rating).ToString("0.00") + userChngTag;
+                                       renameName = _namingLowerPosition + "_" + currentHouse.rating + userChngTag;
                                     }
 
                                     incrementNamingConvention(_namingLowerPosition, false);
